f7dc3b66038dbe6b52e822f643a2e0f42d31428a
Moris8181 <moriskousuke8181@gmail.com>
2022-11-07 21:06:15 +0900

    学部のseedr, 学部、商品,ユーザー紐付け完了

---
+++ app/Models/Commodity.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Commodity extends Model
+{
+    use HasFactory;
+    public function user()
+    {
+    return $this->belongsTo(User::class);
+    }
+    public function faculty()
+    {
+        return $this->belongsTo(Faculty::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+    public function commodities()
+    {
+    return $this->hasMany(Commodity::class);
+    }
 }
