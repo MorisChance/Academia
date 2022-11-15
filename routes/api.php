f290378be3774e14380e3ba206737c7fbb087536
Moris8181 <moriskousuke8181@gmail.com>
2022-11-15 20:56:34 +0900

    API CommodityController完成

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\API\CommodityController;
+use App\Http\Controllers\API\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('commodities', CommodityController::class)
+    ->names('api.commodities');
+
+Route::apiResource('commodities.comments', CommentController::class)
+    ->only(['store', 'show', 'update', 'destroy'])
+    ->names('api.commodities.comments');
