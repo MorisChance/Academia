b9628eb518c9a54e7bbe92aa8d723bd04fa42434
Moris8181 <moriskousuke8181@gmail.com>
2022-11-19 11:33:54 +0900

    laravel完成の修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -411,10 +411,10 @@
     <div class="relative min-h-screen flex ">
         <div
             class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white">
-            <div class="h-full flex flex-auto bg-purple-900 text-white bg-no-repeat bg-cover relative bg-image">
+            <div class="h-full flex flex-auto text-white bg-no-repeat bg-cover relative bg-image">
                 <div
                     class="w-4/5 flex flex-auto flex-col md:flex-row items-center justify-center p-10 xl:p-32 overflow-hidden">
-                    <div class="absolute bg-gradient-to-b from-indigo-900 to-black opacity-20 inset-0 z-0"></div>
+                    <div class="absolute bg-gradient-to-b opacity-20 inset-0 z-0"></div>
                     <div class="w-4/5 z-10">
                         <h2 class="text-xl xl:text-4xl italic mb-2">Academia</h2>
                         <div class="text-5xl sm:text-5xl xl:text-8xl font-bold leading-tight mb-6">最高の教科書を<br>手に入れよう
